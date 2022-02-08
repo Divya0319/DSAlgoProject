25db15a3c81e885bd926cbae5e4da634c4b81e8e
divya0319 <divygupta0319@gmail.com>
2022-02-08 15:20:16 +0530

    Added methods to insert at begin and insert at end

--- DoublyLinkedList_Narsimha.java
+++ DoublyLinkedList_Narsimha.java
@@ -1,6 +1,7 @@
 public class DoublyLinkedList {
     
     private DLLNode head;
+    public int length;
 
     static class DLLNode {
         private int data;
@@ -37,11 +38,92 @@ public class DoublyLinkedList {
             this.prev = prev;
         }
      }
+     
+     public int getLength() {
+         return length;
+     }
+     
+     public DLLNode getHead() {
+         return head;
+     }
     
     public DoublyLinkedList() {
-        head = new DLLNode(Integer.MIN_VALUE);
-    } 
+        length = 0;
+    }
+    
+    public void insertAtBegin(int data) {
+    DLLNode nNode = new DLLNode(data);
+            if(head == null) {
+                head = nNode;
+                nNode.next = null;
+                nNode.prev = null;
+                length++;
+
+                return;
+
+            }
+
+    nNode.setNext(head);
+    head = nNode;
+    nNode.setPrev(null);
+    head.getNext().setPrev(head);
+    length++;
+        
+    }
+    
+    public void insertAtEnd(int data) {
+    DLLNode nNode = new DLLNode(data);
+            if(head == null) {
+                head = nNode;
+                nNode.next = null;
+                nNode.prev = null;
+                length++;
+
+                return;
+
+            }
+
+    DLLNode temp = head;
+    while(temp.getNext() != null) {
+        temp = temp.getNext();
+    }
+    
+    temp.setNext(nNode);
+    nNode.setPrev(temp);
+    nNode.setNext(null);
+    length++;
+        
+    }
+    
+    public String toString() {
+        String result = "[";
+        if(head == null) {
+            return result + "]";
+        }
+        result = result + head.getData();
+        DLLNode temp = head.getNext();
+        while(temp != null) {
+           result = result + ", " + temp.getData();
+           temp = temp.getNext();
+        }
+        
+        return result + "]";
+    }
     public static void main(String args[]) {
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.insertAtBegin(45);
+        System.out.println(list.toString());
+        System.out.println(list.getLength());
+        list.insertAtBegin(5);
+        System.out.println(list.toString());
+        System.out.println(list.getLength());
+        list.insertAtEnd(56);
+        System.out.println(list.toString());
+        System.out.println(list.getLength());
+        list.insertAtEnd(23);
+        System.out.println(list.toString());
+        System.out.println(list.getLength());
+
       
     }
 }
