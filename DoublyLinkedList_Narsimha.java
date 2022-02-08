aa83fbc8da8903817f891d43dcba5b6e160b9ad7
divya0319 <divygupta0319@gmail.com>
2022-02-08 11:51:20 +0530

    Created DOubly linked list implementation, its operation methods remaining

---
+++ DoublyLinkedList_Narsimha.java
@@ -0,0 +1,47 @@
+public class DoublyLinkedList {
+    
+    private DLLNode head;
+
+    static class DLLNode {
+        private int data;
+        private DLLNode prev;
+        private DLLNode next;
+        
+        public DLLNode(int data) {
+            this.data  =data;
+            this.prev = null;
+            this.next = null;
+        }
+        
+        public int getData() {
+            return data;
+        }
+        
+        public void setData(int data) {
+            this.data = data;
+        } 
+        
+        public DLLNode getNext() {
+            return next;
+        }
+        
+        public void setNext(DLLNode next) {
+            this.next = next;
+        }
+        
+        public DLLNode getPrev() {
+            return rpev;
+        }
+        
+        public void setPrev(DLLNode prev) {
+            this.prev = prev;
+        }
+     }
+    
+    public DoublyLinkedList() {
+        head = new DLLNode(Integer.MIN_VALUE);
+    } 
+    public static void main(String args[]) {
+      
+    }
+}
