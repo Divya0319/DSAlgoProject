8f0ac61c7b5f20d49ae0ec8d3d8eca11a6fa0bf3
divya0319 <divygupta0319@gmail.com>
2022-02-26 19:15:41 +0530

    Added comments in ConvertNumToWords program, after understanding the program line by line

--- ConvertNumToWords_Program.java
+++ ConvertNumToWords_Program.java
@@ -7,7 +7,7 @@ public class MyClass {
             numberToWords("5".toCharArray());  
             numberToWords("0".toCharArray());  
             numberToWords("20".toCharArray());  
-            numberToWords("1000".toCharArray());  
+            numberToWords("1000".toCharArray());  // whenever the function sees 0 in the digits other than MSB, then it simply ignores its printing
             numberToWords("12345".toCharArray());  
             //passing empty string   
             numberToWords("".toCharArray());  
@@ -43,37 +43,38 @@ public class MyClass {
         
         /* For single digit number */
         if(len == 1) {
-            System.out.println(oneDigit[num[0] - '0']);
+            System.out.println(oneDigit[num[0] - '0']);  // print single digit number and immediately go out of method
             return;
         }
         
-        int x = 0;
+        int x = 0;    // this variable handles the index of char array of number one by one
         
         //executes until num does not become not '\0'  
-        while(x < num.length) {
+        while(x < num.length) {     // means only run the loop till length of the char array
         
         /* Code path for first 2 digits */
-        if(len >= 3) {
+        if(len >= 3) {   // this code block is only executed for 3 or 4 digit numbers, for 2 digit numbers, block below it is considered
             if(num[x] - '0' != 0) {
                 System.out.print(oneDigit[num[x] - '0'] + " ");
                 System.out.print(poweroftens[len-3] + " ");
                 
             }
-            len--;
+            len--;    // we are decrement length bcz next time when we come, since we have already printed for first digit, we don't need to consider it next time
             
         } 
         /* Code path for last 2 digits */
         else {
     
             if(num[x] -'0' == 1) {
-                int sum = num[x] -'0' + num[x+1] -'0';
+                int sum = num[x] -'0' + num[x+1] -'0';   // sum is performed for printing bcz, for printing 15, we add 1 and 5, getting 6, so printing 6th index from twodigit
+				//array,  which is fifteen
                 System.out.println(twodigits[sum]);
                 
-                return;
+                return;   // we are simply returning because, we are in block in which length 2 is there for number, and so, its printing is finished.
             } 
             /* Need to explicitly handle 20 */
             else if(num[x] -'0' == 2 && num[x+1] - '0' == 0) {
-                System.out.println("Twenty");
+                System.out.println("Twenty");   //Since 20 is in a seperate array, we have to consider it separately
                 
                 return;
             }
@@ -81,16 +82,16 @@ public class MyClass {
                 numbers i.e., 21 to 99 */
             else{
                 int i = (num[x] - '0');
-                if(i > 0) {
+                if(i > 0) {  // this condition ensures that if any 0 is present inside number, not on LSB or MSB, then it should not be considered for printing(bcz in 603, we don't print anything for 0)
                     System.out.print(multipleoftens[i] + " ");
                 }
                 else 
-                    System.out.print("");
+                    System.out.print("");  // printing nothing if 0 is there
                 
-                ++x;
+                ++x; // we are immediately incrementing x here bcz, if 39 is the sub-number, after printing Thirty, we have to print Nine, and so, we have to consider next digit immediately
                 
                 if(num[x] - '0' != 0) 
-                    System.out.println(oneDigit[num[x] - '0']);
+                    System.out.println(oneDigit[num[x] - '0']);  // again, if next digit from left is 0, then we simply ignore it, or else, print the digit's 1 digit word
                 
             }
             
