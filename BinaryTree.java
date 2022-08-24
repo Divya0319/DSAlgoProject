dea19f17a68502a64c7e51c69499c39907917f21
divya0319 <divygupta0319@gmail.com>
2022-08-24 17:29:36 +0530

    Created a class to implement Binary Tree and its nodes, and also created a method to do PreOrder traversal using recursion

---
+++ BinaryTree.java
@@ -0,0 +1,65 @@
+package com.dsalgoproblems.javaproblems;
+
+public class BinaryTree {
+	static class BinaryTreeNode {
+		private int data;
+		private BinaryTreeNode left, right;
+		
+		public BinaryTreeNode(int data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+
+		public int getData() {
+			return data;
+		}
+
+		public void setData(int data) {
+			this.data = data;
+		}
+
+		public BinaryTreeNode getLeft() {
+			return left;
+		}
+
+		public void setLeft(BinaryTreeNode left) {
+			this.left = left;
+		}
+
+		public BinaryTreeNode getRight() {
+			return right;
+		}
+
+		public void setRight(BinaryTreeNode right) {
+			this.right = right;
+		}
+		
+		
+	}
+	
+	private void PreOrder(BinaryTreeNode root) {
+		if(root != null) {
+			System.out.print(root.data + " ");
+			PreOrder(root.left);
+			PreOrder(root.right);
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		BinaryTree bt = new BinaryTree();
+		
+		BinaryTreeNode btn = new BinaryTreeNode(1);
+		btn.left = new BinaryTreeNode(2);
+		btn.right = new BinaryTreeNode(3);
+		btn.left.left = new BinaryTreeNode(4);
+		btn.left.right = new BinaryTreeNode(5);
+		btn.right.left = new BinaryTreeNode(6);
+		btn.right.right = new BinaryTreeNode(7);
+		
+		bt.PreOrder(btn);
+
+	}
+
+}
