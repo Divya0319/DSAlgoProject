b69a0ceb237c6f8e555f0cdc31ae0466f31ca9a0
divya0319 <divygupta0319@gmail.com>
2022-08-28 13:25:28 +0530

    Modified method for postorder traversal using iterative approach, added a simpler way of doing postorder

--- BinaryTreeTraversals.java
+++ BinaryTreeTraversals.java
@@ -94,28 +94,28 @@ public class BinaryTreeTraversals {
 	
 	private ArrayList<Integer> postOrderIterative(BinaryTreeNode root) {
 		ArrayList<Integer> res = new ArrayList<>();
-		if(root == null) 
+		if (root == null)
 			return res;
 		Stack<BinaryTreeNode> s = new Stack<>();
-		s.push(root);
-		BinaryTreeNode prev = null;
-		while(!s.isEmpty()) {
-			BinaryTreeNode curr = s.peek();
-			if(prev == null || prev.left == curr || prev.right == curr) {
-				if(curr.left != null) 
-					s.push(curr.left);
-				else if(curr.right != null) {
-					s.push(curr.right);
-				}
-			} else if(curr.left == prev) {
-				if(curr.right != null) {
-					s.push(curr.right);
-				}
-			} else {
-				res.add(curr.data);
-				s.pop();
+		BinaryTreeNode previous = null;
+		BinaryTreeNode current = root;
+		while(true) {
+			while(current != null) {
+				s.push(current);
+				current = current.left;
 			}
-			prev = curr;
+			if(s.isEmpty()) break;
+			while(current == null && !s.isEmpty()) {
+				current = s.peek();
+				if(current.right == null || current.right == previous) {
+					res.add(current.data);
+					s.pop();
+					previous = current;
+					current = null;
+				} else {
+					current = current.right;
+				}
+			}
 		}
 		return res;
 	}
